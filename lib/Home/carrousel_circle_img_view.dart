2aa60b4559aad1cbfa39e3a58805e477bbdf2f07
Javier Monjaraz <193231@ids.upchiapas.edu.mx>
2022-06-02 10:07:29 -0500

    Slider circle image

--- lib/Home/carrousel_circle_img_view.dart
+++ lib/Home/carrousel_circle_img_view.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
 class CarrouselCircleImg extends StatelessWidget {
-  const CarrouselCircleImg({
+  CarrouselCircleImg({
     Key? key,
     required this.size,
   }) : super(key: key);
 
   final Size size;
+
+  final List images = [
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK5dSkKSwXQgJ6Gtltu5F4yGp0-QnhzA66ig&usqp=CAU",
+    "https://media.traveler.es/photos/613760adcb06ad0f20e11980/master/w_1600%2Cc_limit/202931.jpg",
+    "https://www.lavanguardia.com/files/image_449_220/uploads/2022/04/14/62582316cdc6c.jpeg",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,29 +21,23 @@ class CarrouselCircleImg extends StatelessWidget {
       height: size.height * 0.15,
       // color: Colors.blueGrey,
       child: Container(
-        // color: Colors.black,
-        // decoration: BoxDecoration(
-        //   border: Border.all(
-        //     color: Colors.black,
-        //   ),
-        // ),
-        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         child: ListView.builder(
           itemBuilder: ((context, index) {
             return Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
+              padding: const EdgeInsets.symmetric(horizontal: 5),
               child: CircleAvatar(
                 backgroundImage: NetworkImage(
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK5dSkKSwXQgJ6Gtltu5F4yGp0-QnhzA66ig&usqp=CAU",
+                  images[index],
                 ),
                 child: Center(
-                  child: Text("$index"),
+                  child: Text(""),
                 ),
-                radius: 30,
+                radius: 50,
               ),
             );
           }),
-          itemCount: 20,
+          itemCount: images.length,
           scrollDirection: Axis.horizontal,
         ),
       ),
